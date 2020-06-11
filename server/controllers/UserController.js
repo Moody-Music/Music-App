56db43beaaaad13d3e30b64b0bc59d051760219e
amiruljbr <amiruljabar@gmail.com>
2020-06-11 22:50:52 +0700

    page login and register done

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -6,13 +6,44 @@ const { OAuth2Client } = require('google-auth-library');       //google proced
 const client = new OAuth2Client(process.env.CLIENT_ID);      //google proced
 
 class UserController {
-
+    
     static register(req, res, next){
-
+        let newUser = {
+            email:req.body.email,
+            password:req.body.password            
+        }
+      
+        User.create(newUser)
+        .then(data=>{
+            res.status(201).json(data)
+        })
+        .catch(err=>{
+            res.status(500).json(err)
+        })
     }
 
     static login(req, res, next){
-    
+        let dataUser = {
+            email:req.body.email,
+            password:req.body.password
+        }
+          
+        User.findOne({ where: {email:dataUser.email } } )
+        .then(data =>{
+            if(!data) {
+                next({name:"USER_NOT_FOUND"})
+            } else{
+                if(bcrypt.compareSync(req.body.password, data.password)){
+                let token = jwt.sign({id:data.id,email:data.email}, process.env.SECRET);
+                res.status(200).json({id:data.id,email:data.email,token:token})
+                } else{
+                    next({name:"USER_NOT_FOUND", message:"invalid email / password"})
+                }
+            }
+        })
+        .catch(err =>{
+            next(err)
+        })
     }
 
     static googleUser(req, res, next) {
@@ -53,7 +84,7 @@ class UserController {
             })
         })
         .catch(err => {
-            res.status(500).json({ message: 'Internal Server Error', err })
+            next(err)
         })        
     }
 
